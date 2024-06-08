e644c75d06c34e77bd05cdb3151e63da2a3ba30d
Reza Rezaeemoghadam <rezarezaee469@gmail.com>
2024-06-08 10:54:57 +0330

    modifing test module

--- src/management_library/tests.py
+++ src/management_library/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Book
 # Create your tests here.
+
+class TestAllBooks(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Book.objects.create(title = 'بوستان', author = 'سعدی',ISBN = 10203040, published_date = '1990-06-06', available = True)
+        Book.objects.create(title = 'گلستان', author = 'سعدی',ISBN = 10203040, published_date = '1990-06-06', available = True)
+        cls.books = Book.objects.all()
+
+    def test_first_book(self):
+        self.assertEqual(self.books[0].title, 'بوستان')
+        self.assertEqual(self.books[0].author, 'سعدی')
+        self.assertEqual(self.books[0].ISBN, 10203040)
+        self.assertEqual(self.books[0].published_date.year , 1990)
+
+
+    def test_url_exist(self):
+        response = self.client.get('/all_books/')
+        self.assertEqual(response.status_code, 200) 
+
+    def test_url_exist_by_name(self):
+        response = self.client.get(reverse('all_books'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('all_books'))
+        self.assertTemplateUsed(response, 'all_books.html')
--- src/management_library/urls.py
+++ src/management_library/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import all_books,detail_single_book,all_members,detail_single_member
 
 urlpatterns = [
-    path('all_books',all_books ),
-    path('all_member',all_members ),
+    path('all_books/',all_books , name='all_books'),
+    path('all_member/',all_members ),
     path('detail_book/<pk>',detail_single_book,name='detail_book'),
     path('detail_member/<pk>',detail_single_member,name='detail_member'),
 ]
